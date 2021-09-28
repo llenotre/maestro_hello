c5e49fc0022dffe8d115ab6edd743cd97d8e3156
llenotre <llenotre@student.42.fr>
2021-09-29 00:01:16 +0200

    Removed Cargo

--- src/mod.rs
+++ src/mod.rs
@@ -1,8 +1,13 @@
+//! <Add documentation for your module here>
+
 #![no_std]
+
+extern crate kernel;
 
 use kernel::module::version::Version;
 use kernel::print;
 
+// hello module, version 1.0.0
 kernel::module!("hello", Version::new(1, 0, 0));
 
 #[no_mangle]
